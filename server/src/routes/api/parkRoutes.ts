51c8be1d40bbec47307931d82cc0a4fcd7068352
Chris <cjswindle1971@gmail.com>
2025-03-09 13:53:54 -0700

    models created

--- server/src/routes/api/parkRoutes.ts
+++ server/src/routes/api/parkRoutes.ts
@@ -1,18 +1,18 @@
 import express from 'express';
-import { getWeatherData } from '../api/weatherApi';
+import dotenv from 'dotenv';
+dotenv.config()
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  try {
-    const location = req.query.location as string;
-    if (!location) return res.status(400).json({ error: "Location is required" });
-
-    const data = await getWeatherData(location);
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch weather data' });
-  }
-});
+  const response = await fetch(`${process.env.NPS_API_BASE_URL} `)
+})
+  
 
 export default router;
+
+
+
+let cityLocation = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${process.env.API_KEY}`)
+let cityLocationData = await cityLocation.json()
+let weather = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityLocationData[0].lat}&lon=${cityLocationData[0].lon}&units=imperial&appid=${process.env.API_KEY}`)
