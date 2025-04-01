3d328cedd75e621aafca3529f873c381c9ae2ca7
Charles <knowledgeisking77@gmail.com>
2025-03-31 19:30:11 -0600

    TA help with render

--- server/src/db/config/connection.ts
+++ server/src/db/config/connection.ts
@@ -3,15 +3,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD!,
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+let sequelize;
+
+if(process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL, {
     dialect: "postgres",
-  }
-);
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME!,
+    process.env.DB_USER!,
+    process.env.DB_PASSWORD!,
+    {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      dialect: "postgres",
+    }
+  );
+}
+
 
 export default sequelize;
